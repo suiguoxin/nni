0d85a46e21e2da108cb4dfec063d5af5e7bc6d3a
suiguoxin <suiguoxin@gmail.com>
2019-07-02 13:55:00 +0800

    debug stc kernel, add test code

--- src/sdk/pynni/nni/freezethaw_advisor/kernels.py
+++ src/sdk/pynni/nni/freezethaw_advisor/kernels.py
@@ -22,13 +22,28 @@ kernels.py
 """
 
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+from sklearn.gaussian_process.kernels import StationaryKernelMixin, NormalizedKernelMixin, Kernel, Hyperparameter
 
 
-class KTC():
-    '''
-    Kernel for Training Curve. The kernel given by:
+def _check_length_scale(X, length_scale):
+    length_scale = np.squeeze(length_scale).astype(float)
+    if np.ndim(length_scale) > 1:
+        raise ValueError("length_scale cannot be of dimension greater than 1")
+    if np.ndim(length_scale) == 1 and X.shape[1] != length_scale.shape[0]:
+        raise ValueError("Anisotropic kernel must have the same number of "
+                         "dimensions as data (%d!=%d)"
+                         % (length_scale.shape[0], X.shape[1]))
+    return length_scale
 
-    k(t, t') = beta^alpha/(t+t'+beta)^alpha + delta(t,t')*sigma^2 
+
+class KTC(StationaryKernelMixin, Kernel):
+    '''Kernel for Training Curve.
+
+    The kernel given by:
+
+    k(t, t') = beta^alpha/(t+t'+beta)^alpha + delta(t,t')*sigma^2
 
     Parameters
     ----------
@@ -36,11 +51,107 @@ class KTC():
         Scale mixture parameter
     '''
 
-    def __init__(self, alpha, beta):
+    def __init__(self, alpha=0.5, beta=0.5, alpha_bounds=(1e-5, 1), beta_bounds=(1e-5, 1)):
         self.alpha = alpha
         self.beta = beta
-        self.var = 0
+        self.alpha_bounds = alpha_bounds
+        self.beta_bounds = beta_bounds
 
-    def cov(self, inputs):
-        cov = self.alpha + self.beta
-        return cov
+        # self.hyperparameter_alpha = Hyperparameter("alpha", "numeric", self.alpha_bounds)
+        # self.hyperparameter_beta = Hyperparameter("beta", "numeric", self.beta_bounds)
+
+    @property
+    def hyperparameter_alpha(self):  # pylint:disable=missing-docstring
+        return Hyperparameter(
+            "alpha", "numeric", self.alpha_bounds)
+
+    @property
+    def hyperparameter_beta(self):  # pylint:disable=missing-docstring
+        return Hyperparameter(
+            "beta", "numeric", self.beta_bounds)
+
+    def __call__(self, X, Y=None, eval_gradient=False):
+        """Return the kernel k(X, Y) and optionally its gradient.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples_X, n_features) 
+            Left argument of the returned kernel k(X, Y)
+
+        Y : array, shape (n_samples_Y, n_features), (optional, default=None)
+            Right argument of the returned kernel k(X, Y). If None, k(X, X)
+            if evaluated instead.
+
+        eval_gradient : bool (optional, default=False)
+            Determines whether the gradient with respect to the kernel
+            hyperparameter is determined. Only supported when Y is None.
+
+        Returns
+        -------
+        K : array, shape (n_samples_X, n_samples_Y)
+            Kernel k(X, Y)
+
+        K_gradient : array (opt.), shape (n_samples_X, n_samples_X, n_dims)
+            The gradient of the kernel k(X, X) with respect to the
+            hyperparameter of the kernel. Only returned when eval_gradient
+            is True.
+        """
+        X = np.atleast_2d(X)
+        if Y is not None and eval_gradient:
+            raise ValueError("Gradient can only be evaluated when Y is None.")
+
+        if Y is None:
+            tmp = np.float_power(self.beta, self.alpha)
+            K = np.empty(shape=(X.shape[0], X.shape[0]))
+            for i in range(X.shape[0]):
+                for j in range(X.shape[0]):
+                    K[i, j] = tmp / \
+                        np.float_power(X[i][0] + X[j][0] +  # X is shape(,1) here
+                                       self.beta, self.alpha)
+            if eval_gradient:
+                n_gradient_dim = 2
+                K_gradient = np.zeros((K.shape[0], K.shape[0], n_gradient_dim))
+                for i in range(X.shape[0]):
+                    for j in range(X.shape[0]):
+                        t1 = X[i][0]
+                        t2 = X[j][0]
+                        K_gradient[i, j, 0] = K[i, j] * \
+                            np.log(self.beta / (self.beta + t1 + t2))
+                        K_gradient[i, j, 1] = K[i, j] * \
+                            (t1 + t2 / (self.beta*(self.beta + t1 + t2)))
+                return K, K_gradient
+            else:
+                return K
+        else:
+            # TODO: not used here
+            return np.zeros((X.shape[0], Y.shape[0]))
+
+    def diag(self, X):
+        """Returns the diagonal of the kernel k(X, X).
+
+        The result of this method is identical to np.diag(self(X)); however,
+        it can be evaluated more efficiently since only the diagonal is
+        evaluated.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples_X, n_features)
+            Left argument of the returned kernel k(X, Y)
+
+        Returns
+        -------
+        K_diag : array, shape (n_samples_X,)
+            Diagonal of kernel k(X, X)
+        """
+        X = np.atleast_2d(X)
+
+        tmp = np.float_power(self.beta, self.alpha)
+        diag = np.empty(shape=(X.shape[0]))
+        for i in range(X.shape[0]):
+            diag[i] = tmp / np.float_power(X[i][0] * 2 +
+                                           self.beta, self.alpha)
+        return diag
+
+    def __repr__(self):
+        return "{0}(alpha=[{1}], beta={2})".format(self.__class__.__name__,
+                                                   ", ".join(map("{0:.3g}".format, self.alpha)), self.beta)
---
+++ src/sdk/pynni/nni/freezethaw_advisor/test.py
@@ -0,0 +1,138 @@
+import warnings
+
+import numpy as np
+
+from sklearn.gaussian_process.kernels import Matern, WhiteKernel
+from sklearn.gaussian_process import GaussianProcessRegressor
+
+import matplotlib.pyplot as plt
+
+from nni.freezethaw_advisor.kernels import KTC
+
+
+'''
+t1 = np.array([[1], [2], [3])
+t2 = np.array([[2], [3], [4])
+
+
+ufunc = np.frompyfunc(lambda x, y: x+y, 2, 1)
+
+print(ufunc(2,1))
+
+def outer(X, Y):
+    r = np.empty((X.shape[0], Y.shape[0]))
+    print((X.shape[0], Y.shape[0]))
+    for i, x in enumerate(X):
+        for j, y in enumerate(Y):
+            r[i][j] = x[0] +y
+    return r
+
+
+print(outer(t1, t2))
+
+'''
+
+# pylint:disable=missing-docstring
+# pylint:disable=no-member
+
+
+def ktc_1():
+    x = np.arange(100)
+    for lam in np.arange(0, 1, 0.05):
+        y = np.exp(-x*lam)
+        plt.plot(x, y)
+
+    plt.title('Exponential Decay Basis')
+    plt.savefig('./ktc_1.png')
+    plt.close()
+
+
+def ktc_2():
+    #matern = Matern(nu=2.5)
+    ktc = KTC(alpha=1, beta=0.5)
+    gp = GaussianProcessRegressor(
+        kernel=ktc
+    )
+
+    for _ in range(10):
+        X = np.arange(1, 100).reshape(-1, 1)
+        #K = ktc(X)
+        #y = np.random.multivariate_normal([0]*99, K)
+        mean, cov = gp.predict(X, return_cov=True)
+        y = np.random.multivariate_normal(mean, cov)
+
+        plt.plot(range(1, 100), y)
+
+    plt.title('Samples')
+    plt.savefig('./ktc_2.png')
+    plt.close()
+
+
+def ktc_3():
+    ktc = KTC(alpha=1, beta=0.5) + WhiteKernel(noise_level=1e-4)
+    gp = GaussianProcessRegressor(
+        kernel=ktc
+    )
+
+    for mean_prior in np.arange(0, 0.3, 0.05):
+        # warm up
+        #X_obs = [[1], [2]]
+        #y_obs = [0.9, 0.7]
+        X_obs = [[0]]
+        y_obs = [1]
+        gp.fit(X_obs, y_obs)
+
+        N = 100
+
+        X_s = np.arange(1, N).reshape(-1, 1)
+        mean, cov = gp.predict(X_s, return_cov=True)
+        y_s = np.random.multivariate_normal(mean+mean_prior, cov)
+        y = np.append(y_obs, y_s, axis=0)
+
+        plt.plot(range(0, N), y, label='mean prior:{0:.2g}'.format(mean_prior))
+    plt.title('Training Curve Samples')
+    plt.legend()
+    plt.savefig('./ktc_3.png')
+    plt.close()
+
+
+def ktc_4():
+    ktc = KTC(alpha=1, beta=0.5) + WhiteKernel(noise_level=1e-4)
+    gp = GaussianProcessRegressor(
+        kernel=ktc
+    )
+
+    warm_up = True
+
+    for i in range(1):
+        X = np.empty(shape=(0, 1))
+        y = np.empty(shape=(0))
+
+        if warm_up:
+            warmup_X = [[1], [2], [3], [4], [5]]
+            warmup_y = [1-0.098, 1-0.716, 1-0.830, 1-0.848, 1-0.878]
+            X = np.append(X, warmup_X, axis=0)
+            y = np.append(y, warmup_y, axis=0)
+            gp.fit(X, y)
+
+        for x in range(6, 100):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                mean_prior = 0.1
+                mean, std = gp.predict(np.array([[x]]), return_std=True)
+                y_sample = np.random.normal(
+                    mean+mean_prior, std)  # pylint:disable=no-member
+                X = np.append(X, [[x]], axis=0)
+                y = np.append(y, y_sample, axis=0)
+                gp.fit(X, y)
+
+        plt.plot(range(1, 100), y)
+    plt.title('Training Curve Samples')
+    plt.savefig('./ktc_5.png')
+    plt.close()
+
+
+# ktc_1()
+# ktc_2()
+# ktc_3()
+ktc_4()
