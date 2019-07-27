55a30512d2d4679c9d74d3377938699235baeb05
suiguoxin <suiguoxin@gmail.com>
2019-07-27 17:02:11 +0800

    change RF setting

--- src/sdk/pynni/nni/mtsmac_advisor/predictor.py
+++ src/sdk/pynni/nni/mtsmac_advisor/predictor.py
@@ -38,7 +38,7 @@ class Predictor():
         ----------
         """
         self.regr = RandomForestRegressor(
-            n_estimators=10, max_depth=3, min_samples_split=2, max_features=5/6, random_state=0)
+            n_estimators=10, max_depth=7, min_samples_split=10, max_features=5/6, random_state=0)
         self.multi_task = multi_task
         self.epochs = None
 
